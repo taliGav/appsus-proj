52edbcb7e9f782af1962a5778fe478650bef105b
Tali <taligavrielov@gmail.com>
2022-03-02 12:30:58 +0200

    router; homepage, appsus-app, about-page

---
+++ js/router.js
@@ -0,0 +1,36 @@
+import appsusApp from './pages/appsus-app.cmp.js';
+import homePage from './pages/home-page.cmp.js'
+import aboutPage from './pages/about-page.cmp.js'
+import bookDetails from './pages/book-details.cmp.js';
+import bookDescription from './pages/book-description.cmp.js';
+
+/* <main-app /> */
+
+
+const routes = [
+    {
+        path: '/',
+        component: homePage
+    },
+    {
+        path: '/about',
+        component: aboutPage
+    },
+    {
+        path: '/appsusMain',
+        component: appsusApp
+    },
+    // {
+    //     path: '/book/:bookId',
+    //     component: bookDetails
+    // },
+    // {
+    //     path: '/book/descriction/:bookId',
+    //     component: bookDescription
+    // },
+];
+
+export const router = VueRouter.createRouter({
+    routes,
+    history: VueRouter.createWebHashHistory()
+});
