b021346a4c397ef77e1ed6e73fba8b53795ffb64
Ben <Benl000100@gmail.com>
2022-03-02 12:35:30 +0200

    header button

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -8,7 +8,8 @@ export default {
             <router-link to="/">Home</router-link> |
             <router-link to="/keep">Keep</router-link> |
             <router-link to="/email">Email</router-link> |
-            <router-link to="/book">Books</router-link>
+            <router-link to="/book">Books</router-link> |
+            <router-link to="/about">About</router-link>
         </nav>
     </header>
     `,
