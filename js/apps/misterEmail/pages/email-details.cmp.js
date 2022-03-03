e11fa21d023f9b5de08eb154edec7fd3b102df51
Tali <taligavrielov@gmail.com>
2022-03-03 18:08:22 +0200

    email updates

--- js/apps/misterEmail/pages/email-details.cmp.js
+++ js/apps/misterEmail/pages/email-details.cmp.js
@@ -2,7 +2,7 @@ import { emailService } from "../service/email-service.service.js";
 
 export default {
 
-    props: ['email'],
+    // props: ['email'],
     template: `
         <section class="email-details">
             <p>{{email.from}}</p>
@@ -10,7 +10,8 @@ export default {
             <p>{{email.sentAt}}</p>
             <p>{{email.subject}}</p>
             <p>{{email.body}}</p>
-            <button @click="$emit('close')">back</button>
+            <!-- <button @click="$emit('close')">back</button> -->
+            <!-- <router-link to="/email">Return</router-link> -->
         </section>
         `,
     data() {
@@ -23,11 +24,19 @@ export default {
         this.email.isRead = true;
         emailService.save(this.email);
     },
+    // created() {
+    //     console.log('this.$route is:', this.$route);
+    //     const id = this.$route.params.bookId;
+    //     bookService.get(id)
+    //         .then(book => this.book = book);
+    // },
     methods: {
     },
     computed: {
+
     },
     mounted() {
+        
     },
     unmounted() {
     }
--- js/router.js
+++ js/router.js
@@ -1,8 +1,10 @@
 import appsusApp from './pages/appsus-app.cmp.js';
-import homePage from './pages/home-page.cmp.js'
-import aboutPage from './pages/about-page.cmp.js'
+import homePage from './pages/home-page.cmp.js';
+import aboutPage from './pages/about-page.cmp.js';
 import keepApp from './apps/missKeep/pages/keep-app.cmp.js';
 import emailApp from './apps/misterEmail/pages/email-app.cmp.js';
+import emailCompose from './apps/misterEmail/pages/email-compose.cmp.js';
+import emailDetails from './apps/misterEmail/pages/email-details.cmp.js';
 import bookApp from './apps/missBook/pages/book-app.cmp.js';
 import bookDetails from './apps/missBook/pages/book-details.cmp.js';
 
@@ -26,6 +28,14 @@ const routes = [
     {
         path: '/email',
         component: emailApp
+    },
+    {
+        path: '/email/compose',
+        component: emailCompose
+    },
+    {
+        path: '/email/:emailId',
+        component: emailDetails
     },
     {
         path: '/book',
