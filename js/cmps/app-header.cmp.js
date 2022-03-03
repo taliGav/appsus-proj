6440970310c5f8586d83ebd2985267e29f54acb1
Ben <Benl000100@gmail.com>
2022-03-04 00:18:10 +0200

    header update

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -6,29 +6,37 @@ export default {
                 <img src="./css/img/pagsus-clr.png" alt="logo">
             </router-link>
             <nav class="nav-bar">
-                <img src="./css/img/dots.png" alt="logo">
-                    <router-link  to="/keep"class="keep-link">
-                        <img src="./css/img/keep-bw.png" alt="logo">
+                <router-link  to="/keep"class="keep-link">
+                    <img src="./css/img/keep-bw.png" alt="logo">
                         <img src="./css/img/keep-clr.png" alt="logo">
-                        Keep
+                        <h4>
+                            Keep
+                        </h4>
                     </router-link> |
                     <router-link to="/email" class="email-link">
                         <img src="./css/img/email-bw.png" alt="logo">
                         <img src="./css/img/email-clr.png" alt="logo">
-                        Email
+                        <h4>
+                            Email
+                        </h4>
                     </router-link> |
                     <router-link to="/book" class="books-link">
                         <img src="./css/img/books-bw.png" alt="logo">
                         <img src="./css/img/books-clr.png" alt="logo">
-                        Books
+                        <h4>
+                            Books
+                        </h4>
                     </router-link> |
                     <router-link to="/about" class="about-link">
                         <img class="about-img" src="./css/img/about-bw.png" alt="logo">
                         <img class="about-img" src="./css/img/about-clr.png" alt="logo">
-                        About
+                        <h4>
+                            About
+                        </h4>
                     </router-link>
-        </nav>
-    </header>
-    `,
+                    <img src="./css/img/dots.png" alt="logo">
+                </nav>
+            </header>
+            `,
 
 }
