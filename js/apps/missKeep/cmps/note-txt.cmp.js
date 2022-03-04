0d70b800934d62a8c16102c9535f823c91015f05
Tali <taligavrielov@gmail.com>
2022-03-05 01:21:50 +0200

    update text cmp final - works

--- js/apps/missKeep/cmps/note-txt.cmp.js
+++ js/apps/missKeep/cmps/note-txt.cmp.js
@@ -5,8 +5,6 @@ export default {
             <div class="txt-preview-details" >
                 <h3 class="note-title" @keyup="update" contenteditable="true">{{info.title}}</h3>
                 <p  class="note-text" @keyup="update" contenteditable="true">{{info.txt}}</p>
-                <!-- <h3 class="note-title" @keyup="updateTitle" contenteditable="true">{{info.title}}</h3>
-                <p  class="note-text" @keyup="updateTxt" contenteditable="true">{{info.txt}}</p> -->
             </div>
         </section>`,
     data() {
@@ -17,36 +15,10 @@ export default {
     },
     methods: {
         update(ev) {
-            // console.log('event', ev);
-            // console.log('srcElement:', ev.srcElement);
-            // console.log('target:', ev.target);
-            // console.log('current target:', ev.currentTarget);
-            // console.log('current target:', ev.path[0].localName);
-            // console.log('h3.id',this.id);
-            // console.log('h3.savedValue',this.info.title);
-            // console.log('h3 updated',ev.currentTarget.textContent);
             if (ev.path[0].localName === 'h3') this.newTitle = ev.currentTarget.textContent;
             if (ev.path[0].localName === 'p') this.newTxt = ev.currentTarget.textContent;
             this.$emit('update', this.id, this.newTitle, this.newTxt);
             },
-            // updateTitle(ev){
-            //     console.log('event', ev);
-            //     // console.log('h3.id',this.id);
-            //     // console.log('h3.savedValue',this.info.title);
-            //     // console.log('h3 updated',ev.currentTarget.textContent);
-            //     this.newTitle = ev.currentTarget.textContent;
-            //     // console.log('h3.newTitle',this.newTitle);
-            //     this.$emit('update', this.id, this.newTitle);
-            // },
-            // updateTxt(ev){
-            //     console.log('event', ev);
-            //     console.log('p.id', this.id);
-            //     console.log('p.savedValue', this.info.txt);
-            //     console.log('p updated', ev.currentTarget.textContent);
-            //     this.newTxt = ev.currentTarget.textContent;
-            //     console.log('p.newTitle', this.newTxt);
-            //     this.$emit('update', this.id, this.newTxt);
-            // }
         },
         computed: {
         },
