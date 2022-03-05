9a247c28a9337e06762a9b546b24bde8bb63f40f
Tali <taligavrielov@gmail.com>
2022-03-05 05:11:36 +0200

    img

--- js/apps/missKeep/cmps/note-img.cmp.js
+++ js/apps/missKeep/cmps/note-img.cmp.js
@@ -3,17 +3,23 @@ export default {
     template: `
         <section class="note-preview img-preview">
             <div class="img-preview-details">
-                <h3>{{info.title}}</h3>
+                <h3 class="note-title" @keyup="update" contenteditable="true">{{info.title}}</h3>
                 <img :src="info.url"  />
                 <!-- <img :src="imgUrl"  /> -->
             </div>
         </section>`,
     data() {
         return {
+            newTitle: ''
         };
     },
     methods: {},
     computed: {
+        update(ev) {
+            if (ev.path[0].localName === 'h3') this.newTitle = ev.currentTarget.textContent;
+            this.$emit('update', this.id, this.newTitle);
+            },
+
         // imgUrl() {
         //     return this.info.url;
         // },
